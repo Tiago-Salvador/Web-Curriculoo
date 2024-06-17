82880eb67812022695114c762a421f68884bc0ff
Natanael Tiago <natanaeltiago2566@gmail.com>
2024-06-17 14:25:11 -0300

    Atualização do Profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 // api.js
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Tiago-Salvador/Web-Curriculo/main/assets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Tiago-Salvador/Web-Curr-culo/main/assets/data/profile.json';
     const response = await fetch(url);
     const profileData = await response.json();
     return profileData;
